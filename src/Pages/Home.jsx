4acc9b2c5cc73906f83b9d9b4e8be631ae34414e
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-21 20:00:34 +0200

    add tailwind on pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Search from '../components/Search'
 
 function Home() {
   return (
-    <div>
+    <div
+      className='flex flex-col justify-center items-center text-center'
+      style={{
+        backgroundImage: "url('/images/bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1 className="text-4xl md:text-5xl font-bold text-orange-400 mb-4 mt-6 gap-6">
+        Welcome to Movie Explorer 🎬
+      </h1>
+
+      <p className='text-2xl sm:text-2xl md:text-4xl lg:text-4xl italic leading-relaxed mt-5'>
+        Discover popular movies, explore details, <br />
+        and save your favorites — all in one place!
+      </p>
+
+      <Search />
       
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <nav>
-    
-  <Link to='/'> Home</Link>
-  <Link to='/MovieDetails'> MovieDetails</Link>
-  <Link to=' Favorites'> Favorites</Link>
+    <nav className="w-full flex flex-row justify-between text-white flex-wrap  py-4  space-x-2  bg-amber-900 border-amber-600 rounded-lg shadow-lg  transition-all hover:scale-105 duration-300 ease-in-out  bg-amber-400shadow-lg px-4">
+    <p className="text-2xl sm:text-2xl md:text-4xl lg:text-4xl italic font-bold hover:text-orange-500 transition-all duration-300">Movies</p>
+  <Link to='/'className="'hover:text-orange-500 transition-colors duration-300   hover:text-orange-500 hover:scale-105"> Home</Link>
+  <Link to='/MovieDetails'className="hover:text-orange-300 transition-colors duration-300  hover:text-orange-500  hover:scale-105"> MovieDetails</Link>
+  <Link to='/Favorites' className="hover:text-orange-500 transition-colors duration-300 hover:scale-105"> Favorites</Link>
 
     </nav>
   )

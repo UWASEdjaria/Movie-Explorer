068c27561cbb3a2875480364b32f35ec2e7cc3d6
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-22 23:29:09 +0200

    add search .jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,18 @@
-;
+import React from "react";
 
-function Search() {
+function Search({ searchTerm, setSearchTerm }) {
   return (
     <div className="w-full max-w-md mt-6">
       <input
         type="text"
         placeholder="Search movies..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="w-full text-white p-3 mb-5 rounded-lg outline-none 
                    bg-amber-800 border border-amber-800 
                    shadow-lg focus:ring-2 focus:ring-amber-500 
                    transition-transform duration-300 hover:scale-105"
       />
-
     </div>
   );
 }
